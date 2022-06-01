22fc4d14230e2f200af6a4fc6d2b8caff8322bf7
lmmp77 <lmmp789@gmail.com>
2022-06-01 20:37:33 +0100

    ford fulkerson

--- cmake-build-debug/CMakeFiles/Projeto_DA_2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Projeto_DA_2.dir/DependInfo.cmake
@@ -4,21 +4,13 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/anaca/OneDrive/Ambiente de Trabalho/Projeto_DA_2/Plataform.cpp" "C:/Users/anaca/OneDrive/Ambiente de Trabalho/Projeto_DA_2/cmake-build-debug/CMakeFiles/Projeto_DA_2.dir/Plataform.cpp.obj"
-  "C:/Users/anaca/OneDrive/Ambiente de Trabalho/Projeto_DA_2/main.cpp" "C:/Users/anaca/OneDrive/Ambiente de Trabalho/Projeto_DA_2/cmake-build-debug/CMakeFiles/Projeto_DA_2.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/luispaiva/CLionProjects/Projeto_DA_2/Graph.cpp" "CMakeFiles/Projeto_DA_2.dir/Graph.cpp.o" "gcc" "CMakeFiles/Projeto_DA_2.dir/Graph.cpp.o.d"
+  "/Users/luispaiva/CLionProjects/Projeto_DA_2/Plataform.cpp" "CMakeFiles/Projeto_DA_2.dir/Plataform.cpp.o" "gcc" "CMakeFiles/Projeto_DA_2.dir/Plataform.cpp.o.d"
+  "/Users/luispaiva/CLionProjects/Projeto_DA_2/main.cpp" "CMakeFiles/Projeto_DA_2.dir/main.cpp.o" "gcc" "CMakeFiles/Projeto_DA_2.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
---
+++ cmake-build-debug/CMakeFiles/Projeto_DA_2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Projeto_DA_2.
+# This may be replaced when dependencies are built.
